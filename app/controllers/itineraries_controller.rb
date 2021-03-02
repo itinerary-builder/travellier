7237438716ff6fed03a31207ddc2a22dd527312a
roberto dotti <dotti.roberto.1@gmail.com>
2021-03-02 14:59:39 +0000

    simple-form

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,2 +1,22 @@
 class ItinerariesController < ApplicationController
+    def new
+        @itinerary=Itinerary.new
+    end
+
+    def create
+        @itinerary = Itinerary.new(itinerary_params)
+         @itinerary.user = current_user
+        if @itinerary.save
+            redirect_to itinerary_path(@itinerary)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def itinerary_params
+        params.require(:itinerary).permit(:title, :duration, :city)
+    end
+    
 end
