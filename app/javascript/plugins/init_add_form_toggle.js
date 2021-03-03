14ff9c69c07ae9a1c74c878a591cafaeeb88dfdf
Damon Roberts <damonspencerroberts@gmail.com>
2021-03-03 11:54:21 +0000

    Add exit form btn

--- app/javascript/plugins/init_add_form_toggle.js
+++ app/javascript/plugins/init_add_form_toggle.js
@@ -1,12 +1,20 @@
 const initAddFormToggle = () => {
   const button = document.querySelector('.button');
   const dropdown = document.querySelector('.dropdown-form');
+  const exit = document.querySelector('.dropdown-form-exit');
   if (button) {
     button.addEventListener('click', () => {
       setTimeout(function(){
         dropdown.classList.remove('dropdown-form-hidden');
         button.classList.add('home-button-hidden');
       }, 2000); 
+    });
+  }
+  
+  if (exit) {
+    exit.addEventListener('click', () => {
+      dropdown.classList.add('dropdown-form-hidden');
+      button.classList.remove('home-button-hidden');
     });
   }
 }
