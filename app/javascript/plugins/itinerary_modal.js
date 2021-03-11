440c49a50486a1e60352bad4bbd9692163e4fda0
ryan erswell <ersie93@googlemail.com>
2021-03-11 15:39:03 +0000

    modal changes

--- app/javascript/plugins/itinerary_modal.js
+++ app/javascript/plugins/itinerary_modal.js
@@ -9,7 +9,7 @@ const itineraryModal = () => {
 			const place_name = e.currentTarget.dataset.placeName;
 			const place_link = e.currentTarget.dataset.placeUrl;
 			document.querySelector("#modal-place-name").innerHTML = place_name;
-			document.querySelector("#modal-img").src = photo;
+			document.querySelector("#modal-img").style = `background-image: linear-gradient(rgba(255, 255, 255, 0.4),rgba(0, 0, 0, 0.4)), url(${photo});`;
 			document.querySelector("#modal-place-description").innerHTML = description;
 			document.querySelector("#modal-place-id").value = id;
 			document.querySelector("#modal-place-url").href = place_link;
